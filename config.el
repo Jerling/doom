fded6d1623cb315f96002b33ae632fd96b1d9122
Jerling <jer@lilinjiedeMac.local>
2019-01-09 23:51:49 +0800

    flycheck in c++-mode change to c++11

--- config.el
+++ config.el
@@ -5,3 +5,18 @@
 ;;
 ;; for autocomplete
 (ac-config-default)
+
+;; for eval c++
+;; Use this parameter as C++ default
+(quickrun-add-command "c++/c1z"
+  '((:command . "g++")
+    (:exec    . ("%c -std=c++1z %o -o %e %s"
+		 "%e %a"))
+    (:remove  . ("%e")))
+  :default "c++")
+
+;; flycheck for c++11
+(if (eq system-type 'darwin)
+    (add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++11")))
+  (add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
+    )
