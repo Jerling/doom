94f2bc52738f2b52622f1daab94b6bc3bbb672ec
李林杰 <1161676899@qq.com>
2019-04-20 23:54:01 +0800

    change some thing

--- modules/private/blog/config.el
+++ modules/private/blog/config.el
@@ -7,13 +7,14 @@
   :init
   (setq easy-hugo-basedir +my-blog-root-dir
     easy-hugo-root +my-blog-root-dir
-	easy-hugo-postdir "orgs"
+    easy-hugo-postdir "orgs"
     easy-hugo-url "https://jerling.github.io/"
     easy-hugo-preview-url "http://127.0.0.1:1313/"
     easy-hugo-sshdomain "Jerling"
     easy-hugo-previewtime "20"
     easy-hugo-default-ext ".org"
-    easy-hugo-org-header t)
+    easy-hugo-org-header t
+    )
   (add-hook 'easy-hugo-mode-hook 'cesco/easy-hugo))
 
 (defvar +my-blog-img-dir
